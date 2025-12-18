77d89041c09dc0b4528cb2da51117eedc4ad3d3c
Alencar1992 <lucas.aluncar.l70@gmail.com>
2025-12-18 14:07:06 -0300

    ALENCAR-FRETES 2.0
    VERSÃO ATUALIZADA MAIS COMPLETA

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,37 @@
+const CACHE_NAME = "alencar-fretes-v2";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// INSTALAÇÃO
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("alencar-fretes").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
+// ATIVAÇÃO (limpa cache antigo)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
